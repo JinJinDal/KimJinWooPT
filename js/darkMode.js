6e84382b6a49f44ca73550faf41b30023083f493
KimJinWoo <100827318+JinJinDal@users.noreply.github.com>
2022-11-24 15:16:02 +0900

    미완성용
    111

---
+++ js/darkMode.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+
+    $('input').click(function(){
+
+     if($("input:checked").val() === 'on'){
+         $('.radio-box').addClass('active');
+         $('.inner').addClass('active');
+         $('a').addClass('active');
+         $('#MainSct1').addClass('active');
+         $('body').css({
+             background : 'white',
+             transition: 'all 1s'
+         });
+         console.log('click');
+     }else{
+         $('.radio-box').removeClass('active');
+         $('.inner').removeClass('active');
+         $('a').removeClass('active');
+         $('#MainSct1').removeClass('active');
+         $('body').css('background','black');
+         console.log('noclick');
+     }
+    }); 
+ });
